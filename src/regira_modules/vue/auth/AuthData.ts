64370fe484e50c55459d7ea9d94ed2fc1b5f6cc2
bverboven <bramverboven@hotmail.com>
2024-03-08 19:20:22 +0100

    endings?

--- src/regira_modules/vue/auth/AuthData.ts
+++ src/regira_modules/vue/auth/AuthData.ts
@@ -1,47 +1,48 @@
-type IClaimValue = string | Array<string>
+type IClaimValue = string | Array<string>;
 
 export interface IAuthData {
-    isAuthenticated: boolean
-    expires: number
-    userId?: string
-    name?: string
-    email?: string
-    displayName?: string
-    culture?: string
-    role?: string
+  isAuthenticated: boolean;
+  expires: number;
+  userId?: string;
+  name?: string;
+  email?: string;
+  displayName?: string;
+  culture?: string;
+  role?: string;
 
-    get(claimType: string): IClaimValue | undefined
-    hasPermission(value: string): boolean
+  get(claimType: string): IClaimValue | undefined;
+  hasPermission(value: string): boolean;
 }
 
 export class AuthData implements IAuthData {
-    private _decodedToken: Record<string, any>
-    isAuthenticated: boolean
-    expires: number
-    userId?: string
-    name?: string
-    email?: string
-    displayName?: string
-    culture?: string
-    role?: string | undefined
+  private _decodedToken: Record<string, any>;
+  isAuthenticated: boolean;
+  expires: number;
+  userId?: string;
+  name?: string;
+  email?: string;
+  displayName?: string;
+  culture?: string;
+  role?: string | undefined;
 
-    constructor(token?: string, options: { isAuthenticated: boolean } = { isAuthenticated: false }) {
-        this._decodedToken = token != null ? JSON.parse(window.atob(token.split(".")[1])) : {}
-        this.isAuthenticated = options.isAuthenticated
-        this.expires = (this._decodedToken.exp ?? 0) - (this._decodedToken.nbf ?? 0)
-        this.userId = this.get("sub") as string
-        this.name = this.get("name") as string
-        this.email = this.get("email") as string
-        this.displayName = this.get("displayName") as string
-        this.culture = this.get("culture") as string
-    }
+  constructor(token?: string, options: { isAuthenticated: boolean } = { isAuthenticated: false }) {
+    this._decodedToken = token != null ? JSON.parse(window.atob(token.split(".")[1])) : {};
+    this.isAuthenticated = options.isAuthenticated;
+    this.expires = (this._decodedToken.exp ?? 0) - (this._decodedToken.nbf ?? 0);
+    this.userId = this.get("sub") as string;
+    this.name = this.get("name") as string;
+    this.email = this.get("email") as string;
+    this.displayName = this.get("displayName") as string;
+    this.culture = this.get("culture") as string;
+  }
 
-    get(claimType: string): IClaimValue | undefined {
-        return this._decodedToken[claimType]
-    }
-    hasPermission(value: string): boolean {
-        return this.get("permissions") == value
-    }
+  get(claimType: string): IClaimValue | undefined {
+    return this._decodedToken[claimType];
+  }
+  hasPermission(value: string): boolean {
+    const claimValue = this.get("permissions");
+    return Array.isArray(claimValue) ? claimValue.includes(value) : claimValue == value;
+  }
 }
 
-export default AuthData
+export default AuthData;
