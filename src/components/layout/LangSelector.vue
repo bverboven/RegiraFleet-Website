25731a997ea71d8cd2a08f5413f9f3447077adfb
bverboven <bramverboven@hotmail.com>
2024-03-19 00:20:27 +0100

    french

--- src/components/layout/LangSelector.vue
+++ src/components/layout/LangSelector.vue
@@ -1,7 +1,7 @@
 <template>
     <ul class="list-inline d-inline">
         <li class="list-inline-item cursor pointer" :class="{ 'fw-bold': langCode == 'en' }" @click="setLangCode('en')">EN</li>
-        <!-- <li class="list-inline-item cursor pointer" :class="{ 'fw-bold': langCode == 'fr' }" @click="setLangCode('fr')">FR</li> -->
+        <li class="list-inline-item cursor pointer" :class="{ 'fw-bold': langCode == 'fr' }" @click="setLangCode('fr')">FR</li>
         <li class="list-inline-item cursor pointer" :class="{ 'fw-bold': langCode == 'nl' }" @click="setLangCode('nl')">NL</li>
     </ul>
 </template>
--- src/components/user/translations.ts
+++ src/components/user/translations.ts
@@ -4,73 +4,91 @@ export default {
     changePassword: {
         en: "Change password",
         nl: "Wachtwoord aanpassen",
+        fr: "Changer de mot de passe",
     },
     currentPassword: {
         en: "current password",
         nl: "huidig wachtwoord",
+        fr: "mot de passe actuel",
     },
     fillInUsernameMsg: {
         en: "Fill in username to request a password reset.",
         nl: "Vul je gebruikersnaam in om je wachtwoord opnieuw in te stellen",
+        fr: "Saisissez votre nom d'utilisateur pour demander une réinitialisation de votre mot de passe.",
     },
     forgotPassword: {
         en: "Forgot password?",
         nl: "Wachtwoord vergeten?",
+        fr: "Mot de passe oublié ?",
     },
     newPassword: {
         en: "new password",
         nl: "nieuw wachtwoord",
+        fr: "nouveau mot de passe",
     },
     password: {
         en: "Password",
         nl: "Wachtwoord",
+        fr: "Mot de passe",
     },
     passwordResetReceivedMsg: {
         en: "Password reset request received. Please check your email.",
         nl: "Je aanvraag is ontvangen, gelieve je e-mail te controleren.",
+        fr: "Demande de réinitialisation du mot de passe reçue. Veuillez vérifier votre e-mail.",
     },
     passwordResetSent: {
         en: "Reset request sent",
         nl: "Je aanvraag is verstuurd",
+        fr: "Demande de réinitialisation envoyée",
     },
     passwordResetFailed: {
         en: "Reset request failed",
         nl: "Je aanvraag is mislukt",
+        fr: "Échec de la demande de réinitialisation",
     },
     passwordsMustMatch: {
         en: "Passwords must match.",
         nl: "Wachtwoorden komen niet overeen",
+        fr: "Les mots de passe doivent correspondre.",
     },
     repeatPassword: {
         en: "repeat password",
         nl: "herhaal wachtwoord",
+        fr: "répéter le mot de passe",
     },
     signIn: {
         en: "Sign in",
         nl: "Aanmelden",
+        fr: "Se connecter",
     },
     signOut: {
         en: "Sign out",
         nl: "Afmelden",
+        fr: "Se déconnecter",
     },
     signingIn: {
         en: "Signing in ...",
         nl: "Aan het aanmelden ...",
+        fr: "Connexion en cours...",
     },
     signInInfo: {
         en: "Fill in username & password to sign in",
         nl: "Vul je gebruikersnaam & wachtwoord in",
+        fr: "Saisissez votre nom d'utilisateur et votre mot de passe pour vous connecter.",
     },
     signInErrorMsg: {
         en: "Unfortunately, signing in failed.",
         nl: "Helaas, het aanmelden is mislukt.",
+        fr: "Malheureusement, la connexion a échoué.",
     },
     tryAgainInMin: {
         en: "Try again in {minutes} min.",
         nl: "Probeer opnieuw in {minutes} min.",
+        fr: "Réessayez dans {minutes} min.",
     },
     username: {
         en: "Username",
         nl: "Gebruikersnaam",
+        fr: "Identifiant",
     },
 } as Record<string, ITranslationMessage>
